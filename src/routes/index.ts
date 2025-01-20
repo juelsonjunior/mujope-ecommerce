a5ba1b939cf86d59f59644e624d1084043c2c15b
juelsonjunior <juelsonjunior27@gmail.com>
2025-01-20 13:35:20 +0100

    fix:Corrigindo o nome do metodo do controller da rota product para create

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,7 @@ import produtctController from '../controllers/product/produtctController';
 const router = Router();
 
 //Rout product
-router.get('/product', produtctController.handle);
+router.get('/product', produtctController.create);
 
 //Rout category
 export default router;
