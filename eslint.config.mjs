2174efb286329b60824b33a49e9618cfcf3868f1
juelsonjunior <juelsonjunior27@gmail.com>
2025-01-27 22:26:49 +0100

    chore:Desabilitando a regra de variaveis inutilizadas do eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,19 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import eslintConfigPrettier from "eslint-config-prettier";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  eslintConfigPrettier,
-  {
-    rules:{
-      "@typescript-eslint/no-empty-object-type":"off"
-    }
-  }
+	{ files: ['**/*.{js,mjs,cjs,ts}'] },
+	{ languageOptions: { globals: globals.browser } },
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	eslintConfigPrettier,
+	{
+		rules: {
+			'@typescript-eslint/no-empty-object-type': 'off',
+			'@typescript-eslint/no-unused-vars': 'off',
+		},
+	},
 ];
