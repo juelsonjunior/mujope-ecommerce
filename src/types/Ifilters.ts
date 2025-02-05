9a6db9f06ed07406054a981c7cd43f3e922fc67c
juelsonjunior <juelsonjunior27@gmail.com>
2025-02-05 14:26:21 +0100

    refactor: Melhorando os filtros para serem reutilizaveis

--- src/types/Ifilters.ts
+++ src/types/Ifilters.ts
@@ -1,22 +1,24 @@
-export interface IFilterProduct {
-    name?: string;
-    price?: number;
-    createAtBefore?: string;
-    createAtAfter?: string;
-    updatedAtBefore?: string;
-    updatedAtAfter?: string;
-    sort?: string;
-    page?: string,
-    limit?: string
+import { Prisma } from '@prisma/client';
+
+export interface IFilterItems {
+	name?: string;
+	price?: number;
+	createAtBefore?: string;
+	createAtAfter?: string;
+	updatedAtBefore?: string;
+	updatedAtAfter?: string;
+	sort?:string
+	page?: string;
+	limit?: string;
 }
 
-export interface IFilterCategory {
-	name?: string;
-    createAtBefore?: string;
-    createAtAfter?: string;
-    updatedAtBefore?: string;
-    updatedAtAfter?: string;
-    sort?: string;
-    page?: string,
-    limit?: string
+export interface IFilterModelTypes {
+	Product: {
+		where: Prisma.ProductWhereInput;
+		orderBy: Prisma.ProductOrderByWithRelationInput[];
+	};
+	Category: {
+		where: Prisma.CategoryWhereInput;
+		orderBy: Prisma.CategoryOrderByWithRelationInput[];
+	};
 }
