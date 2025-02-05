e55a09865abedd58e0f0162ef9f2d855c0385d97
juelsonjunior <juelsonjunior27@gmail.com>
2025-02-05 14:25:42 +0100

    refactor:melhorando o sistema de filtro com uma função reutilizavel

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,50 +1,23 @@
 import { Request, Response } from 'express';
-import { IProduct, IFilterProduct, IdParams } from '../types';
+import { IProduct, IFilterItems, IdParams } from '../types';
 import { createProductService } from '../service/';
-import { PrismaClient, Prisma } from '@prisma/client';
-import { parseISO } from 'date-fns';
+import { filterItems } from '../utils/filters';
 
 class ProductController {
-	private prisma: PrismaClient;
+	async index(req: Request<{}, {}, {}, IFilterItems>, res: Response) {
+		const filter = filterItems(
+			req.query,
+			req.query.page,
+			req.query.limit,
+			'Product'
+		);
 
-	constructor() {
-		this.prisma = new PrismaClient();
-	}
-	async index(req: Request<{}, {}, {}, IFilterProduct>, res: Response) {
-		const {
-			name,
-			price,
-			createAtBefore,
-			createAtAfter,
-			updatedAtBefore,
-			updatedAtAfter,
-			sort,
-		} = req.query;
-		const page = req.query.page ? Number(req.query.page) : 1;
-		const limit = req.query.limit ? Number(req.query.limit) : 25;
-		let order: Prisma.ProductOrderByWithRelationInput[] = [];
-		const where: Prisma.ProductWhereInput = {};
-
-		if (name) where.name = { contains: name, mode: 'insensitive',};
-		if (price) where.price = Number(price);
-		
-		if (createAtBefore || createAtAfter) {
-			where.createAt = {};
-			if (createAtBefore) where.createAt.lte = parseISO(createAtBefore);
-			if (createAtAfter) where.createAt.gte = parseISO(createAtAfter);
-		}
-		if (updatedAtBefore || updatedAtAfter) {
-			where.updatedAt = {};
-			if (updatedAtBefore) where.updatedAt.lte = parseISO(updatedAtBefore);
-			if (updatedAtAfter) where.updatedAt.gte = parseISO(updatedAtAfter);
-		}
-		if (sort) {
-			order = sort.split(',').map((item) => {
-				const [field, direction] = item.split(':');
-				return { [field]: direction as 'asc' | 'desc' };
-			});
-		}
-		const result = await createProductService.index(page, limit, where, order);
+		const result = await createProductService.index(
+			filter.page,
+			filter.limit,
+			filter.where,
+			filter.order
+		);
 		res.status(200).json(result);
 	}
 	async show(req: Request, res: Response) {
