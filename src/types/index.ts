828acc9d7a5a057410bc729844eee6322a162c7d
juelsonjunior <juelsonjunior27@gmail.com>
2025-02-15 18:49:15 +0100

    feat:Adicionando o import dos order e customers

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,18 @@
 import { IProduct } from './Iproduct';
 import { IdParams } from './typeParams';
+import { ICustomer } from './Icustomer';
+import { IOrder } from './Iorder';
+import { IOrderItem } from './IorderItems';
 import { IFilterItems, IFilterModelTypes } from './Ifilters';
 import { ICategory } from './Icategory';
 
-export { IProduct, IdParams, IFilterItems, IFilterModelTypes, ICategory };
+export {
+	IProduct,
+	IdParams,
+	IFilterItems,
+	IFilterModelTypes,
+	ICategory,
+	ICustomer,
+	IOrder,
+	IOrderItem,
+};
