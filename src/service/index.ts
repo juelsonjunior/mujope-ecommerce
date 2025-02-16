aa38f8ea3640d519b6e325d81251b45354f98e09
juelsonjunior <juelsonjunior27@gmail.com>
2025-02-16 02:02:39 +0100

    feat:Chamando o arquivo de order service no import

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,5 +1,11 @@
 import createProductService from './createProductService';
 import createCategoryService from './createCategoryService';
 import createCustomerService from './createCustomerService';
+import createOrderService from './createOrderService';
 
-export { createProductService, createCategoryService, createCustomerService };
+export {
+	createProductService,
+	createCategoryService,
+	createCustomerService,
+	createOrderService,
+};
