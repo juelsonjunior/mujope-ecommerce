f5614aa752c5c4fe6dc4c5c74efdf50414197c7a
juelsonjunior <juelsonjunior27@gmail.com>
2025-02-16 02:01:25 +0100

    feat:Chamando o controller do order no arquivo de imprtação

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,5 +1,11 @@
 import categoryController from './categoryController';
 import productController from './productController';
 import customerController from './customerController';
+import orderController from './orderController';
 
-export { productController, categoryController, customerController };
+export {
+	productController,
+	categoryController,
+	customerController,
+	orderController,
+};
